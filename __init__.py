c6aec42bf4daa98f2a172bb93602ff95d9d3c6b7
André C. Santos <andresantos@dei.uc.pt>
2024-03-20 19:44:38 -0400

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,39 @@
+song_dict = {
+    "ac_dc": "../data/original_tracks/ac_dc-back_in_black.mp3",
+    "amy_winehouse": "../data/original_tracks/amy_winehouse_tears_dry_on_their_own_-444172331065337357.mp3",
+    "arctic_monkeys": "../data/original_tracks/arctic_monkeys-the_view_from_the_afternoon.mp3",
+    "amália": "../data/original_tracks/barco_negro_mae_preta_3919739412866929460.mp3",
+    "beatles1": "../data/original_tracks/the_beatles_come_together_-5100490427866381028.mp3",
+    "beatles2": "../data/original_tracks/the_beatles-in_my_life.mp3",
+    "capitão_fausto": "../data/original_tracks/capitao_fausto_morro_na_praia_-6641179691282821506.mp3",
+    "gorillaz": "../data/original_tracks/clint_eastwood_2457717256603709718.mp3",
+    "david_miguel": "../data/original_tracks/david_miguel_inatel_-5439465073308878308.mp3",
+    "ibeyi": "../data/original_tracks/ibeyi_river_-83190806115743851.mp3",
+    "linda_martini": "../data/original_tracks/linda_martini_febril_tanto_mar_8046388705293075872.mp3",
+    "michael_jackson": "../data/original_tracks/michael_jackson_billie_jean_official_video_-5704871459262427122.mp3",
+    "qotsa": "../data/original_tracks/qotsa-no_one_knows.mp3",
+    "queen": "../data/original_tracks/queen-another_one_bites_the_dust.mp3",
+    "slowj": "../data/original_tracks/slowj-mundança.mp3",
+    "strokes": "../data/original_tracks/the_strokes_reptilia_audio_-8485733054794759825.mp3",
+    "white_stripes": "../data/original_tracks/seven_nation_army_-1230753378332656378.mp3"
+}
+
+tapped_dict = {
+    "ac_dc": "../data/tapped/ac_dc-back_in_black-tapped.flac",
+    "amy_winehouse": "../data/tapped/Tears Dry On Their Own - Amy Whinehouse-tapped.flac",
+    "amália": "../data/tapped/Barco Negro (Mão Preta) - Amália Rodrigues-tapped.flac",
+    "arctic_monkeys": "../data/tapped/arctic_monkeys-the_view_from_the_afternoon-tapped.flac",
+    "beatles1": "../data/tapped/Come Together - The Beatles-tapped.flac",
+    "beatles2": "../data/tapped/the_beatles-in_my_life-tapped.flac",
+    "capitão_fausto": "../data/tapped/Morro na Praia - Capitão Fausto-tapped.flac",
+    "david_miguel": "../data/tapped/Inatel - David e Miguel-tapped.flac",
+    "gorillaz": "../data/tapped/Clint Eastwood - Gorillaz-tapped.flac",
+    "ibeyi": "../data/tapped/River - Ibeyi-tapped.flac",
+    "linda_martini": "../data/tapped/Febril - Linda Martini-tapped.flac",
+    "michael_jackson": "../data/tapped/Billie Jean - Michael Jackson-tapped.flac",
+    "qotsa": "../data/tapped/qotsa_no_one_knows-tapped.flac",
+    "queen": "../data/tapped/queen-another_one_bites_the_dust-tapped.flac",
+    "slowj": "../data/tapped/slowj-mundança-tapped.flac",
+    "strokes": "../data/tapped/Reptilia - The Strokes-tapped.flac",
+    "white_stripes": "../data/tapped/white_stripes-seven_nation_army-tapped.flac"
+}
